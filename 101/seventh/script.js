2361a757be6d1b1ac341e01386da1d4f333ff6d5
can101 <cancelikdev@outlook.com>
2023-07-04 00:26:57 +0300

    feat: add timelines and sequencing animations

---
+++ 101/seventh/script.js
@@ -0,0 +1,5 @@
+let tl = gsap.timeline();
+
+tl.to(".green", { x: 600, duration: 2 });
+tl.to(".red", { x: 600, duration: 1 });
+tl.to(".grey", { x: 600, duration: 1 });
---
+++ 101/sixth/script.js
@@ -0,0 +1,16 @@
+gsap.to(".green", {
+  rotation: 360,
+  duration: 1,
+});
+
+gsap.to(".red", {
+  rotation: 360,
+  duration: 1,
+  delay: 1,
+});
+
+gsap.to(".grey", {
+  rotation: 360,
+  duration: 1,
+  delay: 2,
+});
