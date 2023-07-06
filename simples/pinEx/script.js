725ade0d83e3d98362c7d29b429f419e6a8f65d6
can101 <cancelikdev@outlook.com>
2023-07-06 18:21:38 +0300

    feat: add span and pin example

---
+++ simples/pinEx/script.js
@@ -0,0 +1,21 @@
+gsap.registerPlugin(ScrollTrigger);
+
+// Paning
+
+// ScrollTrigger.create({
+//   trigger: ".red",
+//   start: "top top",
+//   end: "+=3000",
+//   pin: true,
+//   //   pinSpacing: ture,
+// });
+
+// gsap.utils.toArray(".section").forEach((panel, i) => {
+//   ScrollTrigger.create({
+//     trigger: panel,
+//     start: "top top",
+//     pin: true,
+//     pinSpacing: false,
+//   });
+// });
+
---
+++ simples/snapEx/script.js
@@ -0,0 +1,17 @@
+gsap.registerPlugin(ScrollTrigger);
+
+// Snapping
+
+let sections = gsap.utils.toArray(".section");
+
+gsap.to(sections, {
+  xPercent: -100 * (sections.length - 1),
+  ease: "none",
+  scrollTrigger: {
+    // trigger: ".container",
+    // pin: true,
+    // scrub: 1,
+    // snap: 1 / (sections.length - 1),
+    // end: () => "+=" + document.querySelector(".container").offsetWidth,
+  },
+});
